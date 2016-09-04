d335af458f335f99f564008948ab04557785fe1c
Andrés Cavazos <andrscvz@gmail.com>
2016-09-04 14:40:52 -0500

    Error 404

--- application/controllers/Contacto.php
+++ application/controllers/Contacto.php
@@ -5,12 +5,11 @@ class Contacto extends CI_Controller {
 
     public function view($page = 'contacto')
     {
-    	/*if ( ! file_exists(APPPATH.'/views/contacto/'.$page.'.php'))
+    	if ( ! file_exists(APPPATH.'/views/contacto/'.$page.'.php'))
         {
-			echo APPPATH.'/views/contacto/'.$page.'.php';
-			// Whoops, we don't have a page for that!
-			show_404();
-        }*/
+                // Whoops, we don't have a page for that!
+                show_404();
+        }
         $this->load->helper('url');
         $data['title'] = ucfirst($page); // Capitalize the first letter
         $this->load->view('contacto/'.$page, $data);
